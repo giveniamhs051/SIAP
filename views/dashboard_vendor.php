a8cb144ecf7846a6a3f313acff714ec72c8d96e7
alchaleesa <alchaleesa@student.ub.ac.id>
2025-10-28 17:55:49 +0700

    Pengaturan di hapus

--- views/dashboard_vendor.php
+++ views/dashboard_vendor.php
@@ -73,12 +73,6 @@
                         </svg>
                         Notifikasi
                     </a>
-                    <a href="#" class="flex items-center px-6 py-3 m-2 text-gray-600 hover:bg-gray-100 rounded-lg">
-                        <svg class="w-6 h-6 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0M3.75 18H7.5m3-6h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0M3.75 12H7.5" />
-                        </svg>
-                        Pengaturan
-                    </a>
                 </nav>
             </div>
 
